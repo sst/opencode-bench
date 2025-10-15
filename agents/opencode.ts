739369064e75c863c0b9b71b0f82008c123df0a3
Frank <frank@sst.dev>
2025-10-15 03:31:24 -0400

    Benchmark GLM-4.6

--- agents/opencode.ts
+++ agents/opencode.ts
@@ -2,14 +2,16 @@ import { createAgent } from "~/lib/createAgent.js";
 
 export const models: string[] = [
   // "opencode/gpt-5",
-  "opencode/gpt-5-codex",
-  "opencode/claude-sonnet-4-5",
+  //"opencode/gpt-5-codex",
+  //"opencode/claude-sonnet-4-5",
   // "opencode/claude-sonnet-4",
   // "opencode/claude-3-5-haiku",
   // "opencode/claude-opus-4-1",
   // "opencode/qwen3-coder",
   // "opencode/grok-code",
   // "opencode/kimi-k2",
+  "opencode/glm-4.6",
+  "opencode/an-g8x",
 ];
 
 // TODO: opencode command to generate a session so the agent runs for each task would share the same context. right now there's no way to generate one but rather only exporting is possible.
